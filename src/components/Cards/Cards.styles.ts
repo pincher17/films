2cead1b0ae884d070ceb52e9b4edd29469320148
andrei <pinchers17@mail.ru>
2022-12-22 23:16:52 +0300

    fix: Cards

--- src/components/Cards/Cards.styles.ts
+++ src/components/Cards/Cards.styles.ts
@@ -3,30 +3,30 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  -webkit-column-gap: 21px;
-  -moz-column-gap: 21px;
-  column-gap: 21px;
+  -webkit-grid-column-gap: 21px;
+  -moz-grid-column-gap: 21px;
+  grid-column-gap: 21px;
   @media (min-width: 1301px) and (max-width: 1499px){
     -webkit-column-gap: 35px;
     -moz-column-gap: 35px;
-    column-gap: 35px;
+    grid-column-gap: 35px;
   }
 
   @media (min-width: 1151px) and (max-width: 1300px){
     -webkit-column-gap: 54px;
     -moz-column-gap: 54px;
-    column-gap: 54px;
+    grid-column-gap: 54px;
   }
 
   @media (max-width: 1150px){
     -webkit-column-gap: 54px;
     -moz-column-gap: 54px;
-    column-gap: 54px;
+    grid-column-gap: 54px;
   }
 
   @media (min-width: 1500px){
     -webkit-column-gap: 21px;
     -moz-column-gap: 21px;
-    column-gap: 21px;
+    grid-column-gap: 21px;
   }
 `
