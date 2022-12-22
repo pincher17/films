350ea0b49fda124a0765341ef70fdfb1227601e1
andrei <pinchers17@mail.ru>
2022-12-22 19:38:56 +0300

    fix: Cards.styles

--- src/components/Cards/Cards.styles.ts
+++ src/components/Cards/Cards.styles.ts
@@ -6,17 +6,25 @@ export const Wrapper = styled.div`
   column-gap: 21px;
   @media (min-width: 1301px) and (max-width: 1499px){
     column-gap: 35px;
+    -webkit-column-gap: 35px;
+    -moz-column-gap: 35px;
   }
 
   @media (min-width: 1151px) and (max-width: 1300px){
     column-gap: 54px;
+    -webkit-column-gap: 54px;
+    -moz-column-gap: 54px;
   }
 
   @media (max-width: 1150px){
     column-gap: 54px;
+    -webkit-column-gap: 54px;
+    -moz-column-gap: 54px;
   }
 
   @media (min-width: 1500px){
     column-gap: 21px;
+    -webkit-column-gap: 21px;
+    -moz-column-gap: 21px;
   }
 `
