ce13303df78297b41042e959bd60369b8508e358
andrei andrei <andrei@MacBook-Air-andrei.local>
2023-06-08 00:06:20 +0300

    resolve conflicts

--- src/App.js
+++ src/App.js
@@ -7,9 +7,28 @@ import FilmPage from './components/FilmPage/FilmPage';
 import SearchPage from './components/searchPage/SearchPage';
 import AllFilmsPage from './components/allFilmsPage/AllFilmsPage';
 import Test from './Containers/Test/Test';
+import { useAppDispatch } from './hook';
+import { setWidthDevice } from './store/widthDeviceSlice';
 
 function App() {
-  
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const updateScreenResolution = () => {
+      dispatch(setWidthDevice(window.innerWidth));
+    };
+
+    // Обновляем разрешение при изменении размеров окна
+    window.addEventListener('resize', updateScreenResolution);
+
+    // Инициализируем разрешение при первоначальной загрузке
+    updateScreenResolution();
+
+    // Отписываемся от события при размонтировании компонента
+    return () => {
+      window.removeEventListener('resize', updateScreenResolution);
+    };
+  }, []);
   return (
     <BrowserRouter>
       <Header />
