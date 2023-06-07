201b82b31f70db8dea4fbbbaa038570d096aec87
andrei andrei <andrei@MacBook-Air-andrei.local>
2023-06-08 00:42:09 +0300

    fix button

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -10,7 +10,6 @@ export const ButtonWrapper = styled.div`
   text-decoration: none;
   border: 1px solid rgb(184, 22, 22);
   border-radius: 5px;
-  margin-bottom: 105px
 ;
   &:active {
           background: rgb(147 14 14);
